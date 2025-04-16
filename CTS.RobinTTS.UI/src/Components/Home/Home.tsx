97d17774bebfecdcced2764a2241759f2782a208
frehmanOnQC <fike.rehman@gmail.com>
2025-04-16 14:40:40 -0500

    fixing up the logo

--- CTS.RobinTTS.UI/src/Components/Home/Home.tsx
+++ CTS.RobinTTS.UI/src/Components/Home/Home.tsx
@@ -1,6 +1,7 @@
 
 import "./Home.css"; // Import CSS file
 import TTSDataGrid from "../TTSDataGrid/TTSDataGrid";
+import robinLogo from '../../assets/Images/RobinLogo.svg';
 
 const Home = () => {
     return (
@@ -8,7 +9,9 @@ const Home = () => {
             {/* Header Section */}
             <header className="header-section">
                 {/* Left: Logo Container */}
-                <div className="logo-container">Robin Text To Speech</div>
+                <div className="logo-container">
+                    <img src={robinLogo} alt="Robin Logo" style={{ maxHeight: '200px', width: 'auto' }} ></img>
+                </div>
 
                 {/* Right: Voice Character Container */}
                 <div className="voice-character-container">Voice Character</div>
